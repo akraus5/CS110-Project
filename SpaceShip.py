a092944e0596315e7c54740f0d05c89be7c97854
Arthur Kraus <akraus5@binghamton.edu>
2017-06-16 10:54:05 -0400

    Update code j16

--- SpaceShip.py
+++ SpaceShip.py
@@ -1,7 +1,12 @@
 class SpaceShip:
-	def __init__(self, x_val, y_val, alive):
-		
+	def __init__(self, x_val = 0, y_val):
+		self.x = x_val
+		self.y = y_val
+		self.alive = True
 
+	def collide():
+		#Health TBD
+		self.alive = False
 
 	def __str__(self):
 		

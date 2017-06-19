3f18488f8b42ac183bd098815482b049db250228
Arthur Kraus <akraus5@binghamton.edu>
2017-06-19 11:55:19 -0400

    quote

--- screen.py
+++ screen.py
@@ -2,28 +2,40 @@ import pygame
 
 class Screen:
 	'''Screen object, used for main menue and the game screen, may or may not be seperated into two seperate objects. '''
-	def __init__(self, x_cen, y_cen, displaying, color):
-		self.screen = pygame.display.set_mode(800,600)
+	def __init__(self, screenSize = (800,600)): #Screen 0,0 is top left
+		self.x_mid = screenSize[0] // 2
+		self.y_mid = screenSize[1] // 2
+		self.screen = pygame.display.set_mode(screenSize[0], screensize[1])
 		pygame.display.color(0,0,0)	#Can us picture of space
 
-		#
-
-	def menuScreen(self):	#Main menue screen, set up GUI for menu
+	
+	#def menuScreen(self):	#Main menue screen, set up GUI for menu
 		#pygame.set_caption('NAME')
+
+	#def gameScreen(self, SpaceShip):
 
 	def frameChange():
 		pygame.display.update()
 
-	def endgame(self, SpaceShip):
-		if (SpaceShip.alive()):
+	#def endgame(self, SpaceShip):
+		#if (SpaceShip.alive()):
 			#Win game
 			#pygame.set_caption('Win')
-
-		else:
+		#else:
 			#Game over, close screen
 			#pygame.set_caption('Lose')
 
 
-			pygame.quit()
+			#pygame.quit()
 
-	def __str__(self, SpaceShip):
+	#def __str__(self, SpaceShip):
+
+
+def testScreen():
+	pygame.init()
+	scrn = Screen()
+	while True:
+		scrn.frameChange()
+
+testScreen()
+

6153c9e9705d3e85df2f07489d79679328820e20
Adam Levine <alevin23@binghamton.edu>
2017-06-23 12:51:04 -0400

    Working on win

--- controller.py
+++ controller.py
@@ -2,6 +2,10 @@
 import obstacle,spaceship,resup
 import pygame
 
+BLACK = (0,0,0)
+RED = (255,0,0)
+WHITE = (255,255,255)
+
 class Controller:
 	"""Main clas of game, all actions occure in this class"""
 
@@ -34,45 +38,45 @@ class Controller:
 		pygame.display.flip()
 
 	def press_c_message(self):
-		self.background.fill(black)
+		self.gameDisplay.blit(self.background,(0,0))
 
-		if self.level == 1
+		if self.level == 1:
 			text = "You are tired from your journey\n do you go to sleep or a party?\n Press 1 to sleep, 2 to party"
 
-		self.message_to_screen(text,red,10,'comicsansms')
+		self.message_to_screen(text,RED,10,'comicsansms')
+		'''
 		for event in pygame.event.get():
 			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:
+					self.GameExit = True
 				if event.key == pygame.K_1:
 					self.initializeObjects()
-					GameLose = False
+					self.GameLose = False
 				if event.key == pygame.K_2:
 					self.initializeObjects()
-					GameLose = False
-		
+					self.GameLose = False
+		'''
 
 	def mainloop(self):
-		black = (0,0,0)
-		red = (255,0,0)
-		white = (255,255,255)
 		#Start Timer
 
 		pygame.key.set_repeat(1,50)
-		GameExit = False
-		GameLose = False
-		GameWin = False
-		while not GameExit:
-			while GameLose == False and GameExit == False:
-				self.background.fill(black)
+		self.GameExit = False
+		self.GameLose = False
+		self.GameWin = False
+		while not self.GameExit:
+			while self.GameLose == False and self.GameWin == False and self.GameExit == False:
+				self.background.fill(BLACK)
 
 				for event in pygame.event.get():
 					if event.type == pygame.QUIT:
-						GameExit = True
+						self.GameExit = True
 					if event.type == pygame.KEYDOWN:
 						self.spaceship.move(event.key)
 				#	elif:
 				#		event.type != pygame.KEYDOWN:
 				#		self.spaceship.move()
-							
+
 
 				self.obstacle.move()
 				self.obstacle.change_dir()
@@ -90,36 +94,43 @@ class Controller:
 				alive,win = self.spaceship.getStatus()
 
 				if not alive:
-					GameLose = True
+					self.GameLose = True
 				if win:
-					GameWin = True
-			if GameLose:
-				self.message_to_screen('You Lose',red,100,'comicsansms')
-				self.message_to_screen('Press C to play again or Q to quit',white,30,'comicsansms',70)
+					self.GameWin = True
+
+			if self.GameLose:
+				self.message_to_screen('You Lose',RED,100,'comicsansms')
+				self.message_to_screen('Press C to play again or Q to quit',WHITE,30,'comicsansms',70)
 
 				for event in pygame.event.get():
 					if event.type == pygame.QUIT:
-						GameExit = True
+						self.GameExit = True
 					if event.type == pygame.KEYDOWN:
 						if event.key == pygame.K_q:
-							GameExit = True
+							self.GameExit = True
 						if event.key == pygame.K_c:
 							self.initializeObjects()
-							GameLose = False
-			if GameWin:
-				self.message_to_screen('You Win',red,100,'comicsansms')
-				self.message_to_screen('Press C to play again or Q to quit',white,30,'comicsansms',70)
+							self.GameLose = False
+
+			if self.GameWin:
+				self.message_to_screen('You Win',RED,100,'comicsansms')
+				self.message_to_screen('Press C to play again or Q to quit',WHITE,30,'comicsansms',70)
 
 				for event in pygame.event.get():
 					if event.type == pygame.QUIT:
-						GameExit = True
+						self.GameExit = True
 					if event.type == pygame.KEYDOWN:
 						if event.key == pygame.K_q:
-							GameExit = True
+							self.GameExit = True
 						if event.key == pygame.K_c:
-							self.message_to_screen('You Lose',red,100,'comicsansms')
+							self.press_c_message()
+
+						if event.key == pygame.K_1:
 							self.initializeObjects()
-							GameWin = False
+							self.GameLose = False
+						if event.key == pygame.K_2:
+							self.initializeObjects()
+							self.GameLose = False
 
 
 

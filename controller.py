8349fd638d6e371ca13061ec0e63583077e239d5
Adam Levine <alevin23@binghamton.edu>
2017-06-23 11:40:17 -0400

    update controller

--- controller.py
+++ controller.py
@@ -12,11 +12,15 @@ class Controller:
 		self.scrH = scrH
 		self.gameDisplay = pygame.display.set_mode((scrW,scrH))
 		self.background = pygame.Surface(self.gameDisplay.get_size()).convert()
+		self.initializeObjects()
+
+		pygame.display.set_caption('Game')
+
+	def initializeObjects(self):
 		self.obstacle = obstacle.Obstacle(self.scrW/2,self.scrH/2,self.scrW,self.scrH,'right',5)
 		self.spaceship = spaceship.SpaceShip(self.scrW,self.scrH,5)
 		self.resup = resup.Resup((self.scrW // 2) , ((self.scrH * 1) // 7))
 		self.sprites = pygame.sprite.RenderPlain((self.spaceship,self.obstacle,self.resup))	#setup main menue, as well as other objects
-		pygame.display.set_caption('Game')
 
 	def message_to_screen(self,text,color,size,font,position=0):
 		self.font = pygame.font.SysFont(font,size)
@@ -31,13 +35,14 @@ class Controller:
 	def mainloop(self):
 		black = (0,0,0)
 		red = (255,0,0)
+		white = (255,255,255)
 		#Start Timer
 
 		pygame.key.set_repeat(1,50)
 		GameExit = False
 		GameLose = False
 		while not GameExit:
-			while GameLose == Flase:
+			while GameLose == False and GameExit == False:
 				self.background.fill(black)
 
 				for event in pygame.event.get():
@@ -63,20 +68,20 @@ class Controller:
 
 				if not alive:
 					GameLose = True
-			if GameLose == True:	
+			if GameLose == True:
 				self.background.fill(black)
 				self.message_to_screen('You Lose',red,100,'comicsansms')
-				self.message_to_screen('Press C to play again or Q to quit',black,70,'comicsansms',30)
-				
+				self.message_to_screen('Press C to play again or Q to quit',white,30,'comicsansms',70)
+
 				for event in pygame.event.get():
 					if event.type == pygame.QUIT:
 						GameExit = True
 					if event.type == pygame.KEYDOWN:
-						if event.key = pygame.K_q:
+						if event.key == pygame.K_q:
 							GameExit = True
+						if event.key == pygame.K_c:
+							self.initializeObjects()
 							GameLose = False
-						if event.key = pygame.K_c:
-							self.mainloop()
 
 
 
